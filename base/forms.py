04e2c7791762a9373d691327a2a631f74264580b
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-03-06 21:48:08 -0500

    Add front-end for Register page

--- base/forms.py
+++ base/forms.py
@@ -4,6 +4,21 @@ from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
     #Use the user creation form
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Enter Your Username',
+            'class': 'form-control-lg form-input-padding'
+
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Enter Your Password',
+            'class': 'form-control-lg form-input-padding'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Enter Your Password Again',
+            'class': 'form-control-lg form-input-padding'
+        })
 
     class Meta:
         model = User
